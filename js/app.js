e9eb1ce5092693e63e79374ee7e01a7b53a648a0
Nbootu <nbootu@me.com>
2022-01-05 16:50:45 +0100

    header complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,31 @@
 'use strict';
 
-console.log('It works');
+/* Switch theme */
+// Get Switch
+const switcher = document.querySelector('.theme-switcher input');
+// Get root
+const doc = document.firstElementChild;
+
+// Check for user prefrence
+if (
+  window.matchMedia &&
+  window.matchMedia('(prefers-color-scheme: dark)').matches
+) {
+  switcher.checked = false;
+} else {
+  switcher.checked = true;
+}
+
+// Listen to change on input
+switcher.addEventListener('input', (e) => {
+  // Check if switch is on
+  const checked = e.target.checked;
+
+  if (!checked) {
+    // Switch is off -> dark mode
+    doc.setAttribute('color-scheme', 'dark');
+  } else {
+    // Switch is on -> light mode
+    doc.setAttribute('color-scheme', 'light');
+  }
+});
